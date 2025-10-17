3523cc696747952d96a148eaaeaf8cfdb775df8a
Tsialone <tsialone1902@gmail.com>
2025-10-17 19:52:58 +0300

    fini sprint_1BIS

---
+++ src/main/java/servlets/FrontServlet.java
@@ -0,0 +1,37 @@
+package servlets;
+
+import java.io.IOException;
+import java.net.URL;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String path = req.getServletPath();
+        ServletContext servletContext = req.getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getNamedDispatcher("default");
+
+        try {
+            boolean ressourceExist = servletContext.getResource(path) != null;
+            if (ressourceExist ) {
+                requestDispatcher.forward(req, resp);
+            } else {
+                resp.setContentType("text/plain");
+                resp.getWriter().println("Hello, ton URL est: " + req.getRequestURI());
+            }
+        } catch (Exception e) {
+            throw e;
+
+        }
+
+    }
+
+}
